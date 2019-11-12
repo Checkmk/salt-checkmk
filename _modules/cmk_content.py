225024603148553a9f2a85f186fa99542199f841
Philipp Lemke <lemke.philipp@gmail.com>
2019-11-12 22:02:26 +0100

    gather only specified graintags if available

--- _modules/cmk_content.py
+++ _modules/cmk_content.py
@@ -29,21 +29,36 @@ def prepare(pillar_file, target, tgt_type=u'pillar', prefix=None, **kwargs):
         salt-call cmk_content.prepare pillar_file=/srv/pillar/grains_for_monitoring.sls target="dbnode*" tgt_type=glob prefix=syndic_x
     '''
     local = salt.client.LocalClient()
-    try:
-        #ret = local.cmd('mk-agent:True', 'grains.items', [], tgt_type)
-        ret = local.cmd(target, 'grains.items', [], tgt_type)
-        #if the minion did not answer, grains will be False, delete those hosts from the ret dict
-        for minion, grains in ret.items():
-            if not grains:
-                del ret[minion]
-        if prefix:
-            pillar = {'mk-host-grains' : { prefix : ret }}
-        else:
-            pillar = {'mk-host-grains' : ret }
 
-        with open(pillar_file, 'w') as output_stream:
-            yaml.dump(pillar, output_stream, default_flow_style=False)
-        return "Grain data from %s hosts gathered" %len(ret)
-    except Exception as e:
-        return pprint.pformat(e)
+    ret = local.cmd(target, 'grains.items', [], tgt_type)
+    graintags = {}
+
+    # try to fetch graintags from pillars
+    try:    
+        grain_tag_list =  __pillar__['cmk-master']['graintags']
+    except:
+        grain_tag_list = False
+
+    #if the minion did not answer, grains will be False, those hosts will be skipped
+    for minion, grains in ret.items():
+        if grains:
+            graintags[minion] = {}
+            if grain_tag_list:
+                for grain in grain_tag_list:
+                    graintags[minion][grain] = str(grains[grain])
+            # Fallback gather all grains from minion
+            else:
+                graintags[minion] = grains
+
+
+    if prefix:
+        pillar = {'mk-host-grains' : { prefix : graintags }}
+    else:
+        pillar = {'mk-host-grains' : graintags }
+
+    with open(pillar_file, 'w') as output_stream:
+        yaml.dump(pillar, output_stream, default_flow_style=False)
+
+    return "Grain data from %s hosts gathered" %len(ret)
+    
 
